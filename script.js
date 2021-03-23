de291bfb946e53c8b56b180d7696d4a4dbe2d5d3
mumbojumbomod <73435823+mumbojumbomod@users.noreply.github.com>
2021-03-23 08:07:24 -0400

    map better

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 class preScene extends Phaser.Scene {
-	constructor(){
-		super({ key: 'preScene' });
+  constructor() {
+    super({ key: 'preScene' });
   }
-    preload(){
-      this.load.spritesheet('codey', 'codey.png', { frameWidth: 72, frameHeight: 90});
-      this.load.image('Z', 'Z.png');
-      this.load.image('circle', '33*33circle.png');
-    }//\n
-      createAnimations() {
-        this.anims.create({
-        key: 'run',
-        frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-      });
-    }
-    create(){
+  preload() {
+    this.load.spritesheet('codey', 'codey.png', { frameWidth: 72, frameHeight: 90 });
+    this.load.image('Z', 'Z.png');
+    this.load.image('circle', '33*33circle.png');
+  }//\n
+  createAnimations() {
+    this.anims.create({
+      key: 'run',
+      frames: this.anims.generateFrameNumbers('codey', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+  }
+  create() {
     this.createAnimations();
     this.add.text(20, 150, 'Endless Shadows', { fontSize: '50px', fill: '#ff00fb' });
     this.add.text(150, 200, 'By Elias', { fontSize: '30px', fill: '#ff00fb' });
@@ -30,57 +30,57 @@ class preScene extends Phaser.Scene {
       this.scene.stop('preScene')
       this.scene.start('Level')
       gameState.player.anims.play('run', false);
-    });    
-    }
-    update(){
-    }
+    });
+  }
+  update() {
+  }
 }
 class dead extends Phaser.Scene {
-	constructor(){
-		super({ key: 'dead' });
+  constructor() {
+    super({ key: 'dead' });
   }
-    preload(){
-    }//\n
+  preload() {
+  }//\n
 
-    create(){
-      function chooseDeathString(){
+  create() {
+    function chooseDeathString() {
       let bingo = Math.floor(Math.random() * 5)
       let stringArray = ['After failing so miserably, and with\n your soul ripped from your chest,\n you have nothing left do but to wander\n around and turn into nothing more than\n a shadow.', 'As the light was sucked from your body,\n the darkness, with nothing to hold it\n back, fully took hold over the world.', 'As you died, your bright soul turned\n a shade of black, and dissolved\n into nothing.', 'The darkness, freed from its eternal\n prison, has now comlpetely eradicated\n happiness and light from the world.', 'As your eternal light is finally\n extinguished, you realize one important\n fact: That your life was meaningless,\n because you accomplished nothing.\n                            -b00kNerd']
       return stringArray[bingo]
-      }
+    }
     this.add.text(20, 150, chooseDeathString(), { fontSize: '20px', fill: '#ff00fb' });
     this.add.text(120, 300, 'Click to try again', { fontSize: '20px', fill: '#ff00fb' });
     this.input.on('pointerup', () => {
       this.scene.stop('dead')
       this.sound.stopAll();
       this.scene.start('Level')
-    });    
-    }
-    update(){
-    }
+    });
+  }
+  update() {
+  }
 }
 class win extends Phaser.Scene {
-	constructor(){
-		super({ key: 'win' });
+  constructor() {
+    super({ key: 'win' });
   }
-  preload(){
+  preload() {
   }//\n
-  create(){
-  this.add.text(20, 150, 'As you raise the relic, your soul\n combines with its power, the darkness\n dissolves away with its spikes.\n The souls who have been drained revert\n to their original state.\n The world is once again full of color.', { fontSize: '20px', fill: '#ff00fb' });
-  this.add.text(120, 300, 'Click to play again', { fontSize: '20px', fill: '#ff00fb' });
-  this.input.on('pointerup', () => {
-    this.scene.stop('win')
-    this.scene.start('Level')
-  });    
+  create() {
+    this.add.text(20, 150, 'As you raise the relic, your soul\n combines with its power, the darkness\n dissolves away with its spikes.\n The souls who have been drained revert\n to their original state.\n The world is once again full of color.', { fontSize: '20px', fill: '#ff00fb' });
+    this.add.text(120, 300, 'Click to play again', { fontSize: '20px', fill: '#ff00fb' });
+    this.input.on('pointerup', () => {
+      this.scene.stop('win')
+      this.scene.start('Level')
+    });
   }
-    update(){
-    }
+  update() {
+  }
 }
 
 class Level extends Phaser.Scene {
 
   constructor(key) {
-		super({ key: 'Level' });
+    super({ key: 'Level' });
     this.levelKey = key
     this.nextLevel = {
       'Level1': 'Level2',
@@ -93,8 +93,8 @@ class Level extends Phaser.Scene {
     this.load.audio('jump', 'noise/death.mp3');
     this.load.image('platform', 'platform-1.png');
     this.load.spritesheet('campfire', 'campfire.png',
-      { frameWidth: 32, frameHeight: 32});
-    this.load.spritesheet('codey', 'codey.png', { frameWidth: 72, frameHeight: 90});
+      { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('codey', 'codey.png', { frameWidth: 72, frameHeight: 90 });
     this.load.image('blaad', 'blaad.png');
     this.load.image('bg1', 'trees.png');
     this.load.image('bg2', 'mountain.png');
@@ -120,10 +120,10 @@ class Level extends Phaser.Scene {
     gameState.player = this.physics.add.sprite(gameState.spawnPointX, gameState.spawnPointY, 'codey').setScale(.5)//20, 100
     gameState.spawnPointX = 20;
     gameState.spawnPointY = 100;
-      gameState.player.anims.play('run', false);
-      gameState.player.body.setSize(gameState.player.width-40, gameState.player.height - 50).setOffset(20, 50);
-      gameState.player.setBounce(.2)
-      gameState.playerLight = this.lights.addLight(0, 0, 100, 0xff00e6, 3.3)
+    gameState.player.anims.play('run', false);
+    gameState.player.body.setSize(gameState.player.width - 40, gameState.player.height - 50).setOffset(20, 50);
+    gameState.player.setBounce(.2)
+    gameState.playerLight = this.lights.addLight(0, 0, 100, 0xff00e6, 3.3)
     gameState.platforms = map.createLayer('platforms', tileset, 0, 0).setPipeline('Light2D')//.setScale(0.9);
     gameState.platforms.setCollisionByExclusion(-1, true);
     this.physics.add.collider(gameState.player, gameState.platforms);
@@ -140,94 +140,98 @@ class Level extends Phaser.Scene {
     gameState.cursors = this.input.keyboard.createCursorKeys();
 
 
-////spikes
-  this.spikes = this.physics.add.group({
-    allowGravity: false,
-    immovable: true,
-  });
-  const spikeObjects = map.getObjectLayer('spikes')['objects'];
-  spikeObjects.forEach(spikeObject => {
-  const spike = this.spikes.create(spikeObject.x, spikeObject.y - spikeObject.height, 'spikes').setOrigin(0, 0).setPipeline('Light2D');
-  });
-  this.physics.add.overlap(gameState.player, this.spikes, () => { 
-    gameState.player.body.destroy();
-    this.anims.pauseAll();
-    trapblood()
-    let timer = this.time.addEvent({
-      delay: 500,
-      callback: () => {this.physics.pause();
-      this.anims.resumeAll();
-      //gameState.jump.play();
-    this.cameras.main.shake(300, .05, false, function(camera, progress) {
-      if (progress > .9) {
-      this.scene.stop('Level')
-      this.scene.start('dead')
-      }
-  })}
-  })       
- });
-  gameState.particles = this.add.particles('blaad')
-/////spikes
-  function trapblood() {
-    gameState.emitter = gameState.particles.createEmitter({
+    ////spikes
+    this.spikes = this.physics.add.group({
+      allowGravity: false,
+      immovable: true,
+    });
+    const spikeObjects = map.getObjectLayer('spikes')['objects'];
+    spikeObjects.forEach(spikeObject => {
+      const spike = this.spikes.create(spikeObject.x, spikeObject.y - spikeObject.height, 'spikes').setOrigin(0, 0).setPipeline('Light2D');
+    });
+    this.physics.add.overlap(gameState.player, this.spikes, () => {
+      gameState.player.body.destroy();
+      this.anims.pauseAll();
+      trapblood()
+      let timer = this.time.addEvent({
+        delay: 500,
+        callback: () => {
+          this.physics.pause();
+          this.anims.resumeAll();
+          //gameState.jump.play();
+          this.cameras.main.shake(300, .05, false, function (camera, progress) {
+            if (progress > .9) {
+              this.scene.stop('Level')
+              this.scene.start('dead')
+            }
+          })
+        }
+      })
+    });
+    gameState.particles = this.add.particles('blaad')
+    /////spikes
+    function trapblood() {
+      gameState.emitter = gameState.particles.createEmitter({
 
         x: gameState.player.x,
-        y: gameState.player.y+10,
-        speedX: { min:-30, max: 30 },
+        y: gameState.player.y + 10,
+        speedX: { min: -30, max: 30 },
         speedY: { min: 50, max: 200 },
         scale: { start: 1, end: 0 },
         lifespan: 600,
         frequency: 25,
         quantity: 0.5,
         gravityY: 200,
-    })
-  }
-  gameState.pain = function (x, y) {
-    gameState.pained = gameState.particles.createEmitter({
+      })
+    }
+    gameState.pain = function (x, y) {
+      gameState.pained = gameState.particles.createEmitter({
 
         x: x,
         y: y,
-        speedX: { min:-30, max: 30 },
+        speedX: { min: -30, max: 30 },
         speedY: { min: 50, max: 200 },
         scale: { start: 1, end: 0 },
         lifespan: 600,
         frequency: 25,
         quantity: 0.5,
         gravityY: 200,
-    })
-  }
-  this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#000000"); 
-  ///theshadow
-  gameState.shadow = this.physics.add.group({
-    allowGravity: false,
-    immovable: true,
-  });
-  gameState.shadowobject = map.getObjectLayer('shadow')['objects'];
-  gameState.shadowobject.forEach(shadowject => {
-  gameState.shadowconst = gameState.shadow.create(shadowject.x, shadowject.y - shadowject.height, 'lightlessSoul').setOrigin(0, 0).setPipeline('Light2D')
-  gameState.shadowconst.y -= 15;
-  });
-  this.physics.add.overlap(gameState.player, gameState.shadow, () => { 
-    gameState.player.body.destroy();
-    this.anims.pauseAll();
-    trapblood()
-    let timer = this.time.addEvent({
-      delay: 500,
-      callback: () => {this.physics.pause();
-      this.anims.resumeAll();
-      //gameState.jump.play();
-    this.cameras.main.shake(300, .05, false, function(camera, progress) {
-      if (progress > .9) {
-      this.scene.stop('Level')
-      this.scene.start('dead')
-      }
-  })}
-  })       
- });
-  gameState.shadows = [];//array of enemies
-  gameState.shadow.children.iterate(function (child) {
-    gameState.shadows.push(child)
-  });
+      })
+    }
+    this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor("#000000");
+    ///theshadow
+    gameState.shadow = this.physics.add.group({
+      allowGravity: false,
+      immovable: true,
+    });
+    gameState.shadowobject = map.getObjectLayer('shadow')['objects'];
+    gameState.shadowobject.forEach(shadowject => {
+      gameState.shadowconst = gameState.shadow.create(shadowject.x, shadowject.y - shadowject.height, 'lightlessSoul').setOrigin(0, 0).setPipeline('Light2D')
+      gameState.shadowconst.y -= 15;
+    });
+    this.physics.add.overlap(gameState.player, gameState.shadow, () => {
+      gameState.player.body.destroy();
+      this.anims.pauseAll();
+      trapblood()
+      let timer = this.time.addEvent({
+        delay: 500,
+        callback: () => {
+          this.physics.pause();
+          this.anims.resumeAll();
+          //gameState.jump.play();
+          this.cameras.main.shake(300, .05, false, function (camera, progress) {
+            if (progress > .9) {
+              this.scene.stop('Level')
+              this.scene.start('dead')
+            }
+          })
+        }
+      })
+    });
+    gameState.shadows = [];//array of enemies
+    gameState.shadow.children.iterate(function (child) {
+      gameState.shadows.push(child)
+    });
     gameState.moveTween = this.tweens.add({
       targets: gameState.shadows,
       x: '-=60',
@@ -237,8 +241,8 @@ class Level extends Phaser.Scene {
       repeat: -1,
       yoyo: true
     });
- //theshadow  
-}
+    //theshadow  
+  }
 
   createAnimations() {
     this.anims.create({
@@ -302,28 +306,28 @@ class Level extends Phaser.Scene {
 
   levelSetup() {
     // Create the campfire at the end of the level
-      gameState.hallowed_light = this.add.particles('light')
-/////spikes
-gameState.thing = this
-    gameState.lightSpray = function() {
+    gameState.hallowed_light = this.add.particles('light')
+    /////spikes
+    gameState.thing = this
+    gameState.lightSpray = function () {
       gameState.emitter = gameState.hallowed_light.createEmitter({
-          x: gameState.player.x,
-          y: gameState.player.y+10,
-          speedX: { min:-200, max: 200 },
-          speedY: { min: -200, max: 200 },
-          scale: { start: 1, end: 0 },
-          lifespan: 600,
-          frequency: 25,
-          quantity: 0.5,
+        x: gameState.player.x,
+        y: gameState.player.y + 10,
+        speedX: { min: -200, max: 200 },
+        speedY: { min: -200, max: 200 },
+        scale: { start: 1, end: 0 },
+        lifespan: 600,
+        frequency: 25,
+        quantity: 0.5,
 
-      })             
+      })
     }
-    gameState.goal = this.lights.addLight(gameState.width - 40+10, 110, 200, 0xff00e6, 6.3)
-    gameState.lantern = this.physics.add.sprite(gameState.width - 40+16, 9.9, 'lantern')//.setScale(1);
+    gameState.goal = this.lights.addLight(gameState.width - 40 + 10, 110, 200, 0xff00e6, 6.3)
+    gameState.lantern = this.physics.add.sprite(gameState.width - 40 + 16, 9.9, 'lantern')//.setScale(1);
     this.physics.add.collider(gameState.lantern, gameState.platforms);
-    this.physics.add.overlap(gameState.player, gameState.lantern, function() {
+    this.physics.add.overlap(gameState.player, gameState.lantern, function () {
       gameState.lightSpray()
-      this.cameras.main.fade(800, 255, 107, 233, false, function(camera, progress) {
+      this.cameras.main.fade(800, 255, 107, 233, false, function (camera, progress) {
         if (progress >= 1) {
           this.scene.stop('Level')
           this.scene.start('win')
@@ -333,7 +337,7 @@ gameState.thing = this
       /*gameState.spawnPointX = 1950;
       gameState.spawnPointY = 100;*/
     }, null, this);
-    
+
   }
 
   update() {
@@ -341,12 +345,12 @@ gameState.thing = this
       //gameState.pain(child.x, child.y)
     });
 
-    if(gameState.active){
+    if (gameState.active) {
       if (gameState.cursors.right.isDown) {
         gameState.player.flipX = false;
         gameState.player.setVelocityX(gameState.speed);
         gameState.player.anims.play('run', true);
-      } else if (gameState.cursors.left.isDown ) {
+      } else if (gameState.cursors.left.isDown) {
         gameState.player.flipX = true;
         gameState.player.setVelocityX(-gameState.speed);
         gameState.player.anims.play('run', true);
@@ -362,7 +366,7 @@ gameState.thing = this
         //this.sound.stopAll();
       }
 
-      if (!gameState.player.body.onFloor()){
+      if (!gameState.player.body.onFloor()) {
         gameState.player.anims.play('jump', true);
         //this.cameras.main.shake(100, .01, false, function(camera, progress){})
       }
@@ -370,10 +374,10 @@ gameState.thing = this
       if (gameState.player.y > gameState.bg3.height) {
         this.physics.pause();
         //gameState.jump.play();
-        this.cameras.main.shake(1000, .01, false, function(camera, progress) {
+        this.cameras.main.shake(1000, .01, false, function (camera, progress) {
           if (progress > .9) {
-          this.scene.stop('Level')
-          this.scene.start('dead')
+            this.scene.stop('Level')
+            this.scene.start('dead')
           }
         });
       }
@@ -390,8 +394,8 @@ const config = {
   type: Phaser.WEBGL,
   width: 500,
   height: 600,
-  scale: {mode: Phaser.Scale.FIT},
-  fps: {target: 60},
+  scale: { mode: Phaser.Scale.FIT },
+  fps: { target: 60 },
   backgroundColor: "#000000",
   physics: {
     default: 'arcade',
